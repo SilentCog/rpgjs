d0cad2114a829d2caf63b6b3ded4e0e97572d4fb
Mimi Flynn <mimiflynn@gmail.com>
2015-03-09 20:40:30 -0600

    optimizations

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ function Game () {
 
   // private methods
   var options = {
-    cardinalDirections: [
+    cardinalDirections: {
+      prompt: 'What direction should do go? ',
+      options: [
       'North',
       'South',
       'East',
       'West'
-    ],
-    status: [
+    ]},
+    status: {
+      prompt: 'What should you do? ',
+      options: [
       'Sleep',
       'Coffee',
       'Drive'
-    ]
+    ]}
   };
 
   var utils = {
@@ -31,41 +35,42 @@ function Game () {
       } else {
         next();
       }
+    },
+    prompt: function (option) {
+      console.log(option.prompt + '(' + utils.outputChoices(option.options) + ')');
     }
   }
 
   var directions = function () {
-    console.log('To make a move use player.move()');
+    console.log('To make a move use player.move(). Refresh to start again.\n---------------------------------------------------------\n');
   };
 
-  var answer = '',
+  var answer,
       next,
-      die = 'You die in a horrific accident. Too bad about your life.';
-
-  var prompt = function (arr) {
-    console.log('What direction should do go? (' + utils.outputChoices(arr) + ')');
-  };
+      die;
 
   var start = function () {
-    console.log('You have to drive a truck with a trailer across the country from New York to Colorado in the winter.');
-    prompt(options.cardinalDirections);
+    console.log('You have to drive a truck with a trailer across the country from New York to Colorado in the winter.\nThere is a blizzard to the West.');
+    utils.prompt(options.cardinalDirections);
     answer = 'South';
+    die = 'White out conditions. Snow piles up beyond the tail pipe. You die sleepily in your vehicle awaiting rescue.';
     next = step2;
   };
 
   var step2 = function () {
     console.log('You get to Washington DC and the roads are fairly dry.');
-    prompt(options.cardinalDirections);
+    utils.prompt(options.cardinalDirections);
     answer = 'West';
+    die = 'You die in a horrific accident. Too bad about your life.';
     next = step3;
   };
 
   var step3 = function () {
     console.log('You get to the foothills of the Appalachians after a long day. ');
-    prompt(options.status);
+    utils.prompt(options.status);
     answer = 'Sleep';
-    next = step3;
     die = 'You tumble off the highway and are never seen again.';
+    next = step3;
   };
 
   // public methods
@@ -80,8 +85,8 @@ function Game () {
   }
 
   this.init = function () {
-    start();
     directions();
+    start();
   };
 
 };
