b7c0bf51804143375cecc472f174bef127d87329
Mimi Flynn <mimiflynn@gmail.com>
2015-03-10 08:42:14 -0600

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
-function Game () {
+function Game (name, options) {
 
-  // private methods
-  var options = {
+  var actions = options.actions,
+      steps = options.steps,
+      count = 0,
+      correctAnswer,
+      next,
+      causeOfDeath;
+
+  var utils = {
+    outputChoices: function (arr) {
+      var output = '',
+          length = arr.length - 1;
+      arr.forEach(function (e, i, a) {
+        output += (i !== length) ? e + ', ' : e;
+      });
+      return output;
+    },
+    readInput: function (input) {
+      console.log(input);
+      if (input !== correctAnswer) {
+        console.log(causeOfDeath);
+      } else {
+        play(count);
+      }
+    },
+    showPrompt: function (option) {
+      console.log(option.prompt + '(' + utils.outputChoices(option.options) + ')');
+    }
+  };
+
+  var play = function (move) {
+    var step = steps[count];
+    correctAnswer = actions[step.prompt].options[step.answer];
+    causeOfDeath = step.die;
+    console.log(step.message);
+    if (prompt) utils.showPrompt(actions[step.prompt]);
+    count++;
+  };
+
+  var directions = function () {
+    console.log(name + '\n' + 'To make a move use player.move(). Refresh to start again.\n---------------------------------------------------------\n');
+  };
+
+  // public methods
+
+  this.player = {
+    move: function (input) {
+      utils.readInput(input);
+    },
+    end: function () {
+      console.log('quitter');
+    }
+  };
+
+  this.init = function () {
+    directions();
+    play(count);
+  };
+
+};
+
+var game = new Game('Westward Bound', {
+  actions:{
     cardinalDirections: {
       prompt: 'What direction should do go? ',
       options: [
@@ -27,101 +87,37 @@ function Game () {
         'Gas station snacks'
       ]
     }
-  };
+  },
+  steps: [{
+    message: 'You have to drive a truck with a trailer across the country from New York to Colorado in the winter.\nThere is a blizzard to the West.',
+    prompt: 'cardinalDirections',
+    answer: 1,
+    die: 'White out conditions. Snow piles up beyond the tail pipe. You die sleepily in your vehicle awaiting rescue.'
+  }, {
+    message: 'You get to Washington DC and the roads are fairly dry.',
+    prompt: 'cardinalDirections',
+    answer: 3,
+    die: 'You die in a horrific accident. Too bad about your life.'
+  }, {
+    message: 'You get to the foothills of the Appalachians after a long day. ',
+    prompt: 'driverStatus',
+    answer: 0,
+    die: 'You tumble off the highway and are never seen again.'
+  }, {
+    message: 'You awake. Well rested but hungry. ',
+    prompt: 'restaurants',
+    answer: 1,
+    die: 'You lose a day to indigestion.'
+  }, {
+    message: 'The mountains are now behind you and the Great Plains stretch toward the horizon.\n ',
+    prompt: 'cardinalDirections',
+    answer: 1,
+    die: 'You are going the wrong way. Refer to a map.'
+  }, {
+    message: 'Welcome to your new life in the Rockies. \nMake wise decisions. \n"Live long and prosper"'
+  }]
+});
 
-  var utils = {
-    outputChoices: function (arr) {
-      var output = '',
-          length = arr.length - 1;
-      arr.forEach(function (e, i, a) {
-        output += (i !== length) ? e + ', ' : e;
-      });
-      return output;
-    },
-    readInput: function (input) {
-      console.log(input);
-      if (input !== answer) {
-        console.log(die);
-      } else {
-        next();
-      }
-    },
-    prompt: function (option) {
-      console.log(option.prompt + '(' + utils.outputChoices(option.options) + ')');
-    }
-  }
-
-  var directions = function () {
-    console.log('To make a move use player.move(). Refresh to start again.\n---------------------------------------------------------\n');
-  };
-
-  var answer,
-      next,
-      die;
-
-  // refactor
-  var start = function () {
-    console.log('You have to drive a truck with a trailer across the country from New York to Colorado in the winter.\nThere is a blizzard to the West.');
-    utils.prompt(options.cardinalDirections);
-    answer = options.cardinalDirections.options[1];
-    die = 'White out conditions. Snow piles up beyond the tail pipe. You die sleepily in your vehicle awaiting rescue.';
-    next = step2;
-  };
-
-  var step2 = function () {
-    console.log('You get to Washington DC and the roads are fairly dry.');
-    utils.prompt(options.cardinalDirections);
-    answer = options.cardinalDirections.options[3];
-    die = 'You die in a horrific accident. Too bad about your life.';
-    next = step3;
-  };
-
-  var step3 = function () {
-    console.log('You get to the foothills of the Appalachians after a long day. ');
-    utils.prompt(options.driverStatus);
-    answer = options.driverStatus.options[0];
-    die = 'You tumble off the highway and are never seen again.';
-    next = step4;
-  };
-
-  var step4 = function () {
-    console.log('You awake. Well rested but hungry. ');
-    utils.prompt(options.restaurants);
-    answer = options.restaurants.options[2];
-    die = 'You lose a day to indigestion.';
-    next = step5;
-  };
-
-  var step5 = function () {
-    console.log('The mountains are now behind you and the Great Plains stretch toward the horizon.\n ');
-    utils.prompt(options.cardinalDirections);
-    answer = options.cardinalDirections.options[3];
-    die = 'You are going the wrong way.';
-    next = finish;
-  };
-
-  var finish = function () {
-    console.log('Welcome to your new life in the Rockies. \nMake wise decisions. \n"Live long and prosper"');
-  };
-
-  // public methods
-
-  this.player = {
-    move: function (input) {
-      utils.readInput(input);
-    },
-    end: function () {
-      console.log('quitter');
-    }
-  }
-
-  this.init = function () {
-    directions();
-    start();
-  };
-
-};
-
-var game = new Game();
 var player = game.player;
+
 (window.console) ? game.init() : document.getElementsByTagName('h1')[0].innerHTML = 'Gotta use a different browser.';
