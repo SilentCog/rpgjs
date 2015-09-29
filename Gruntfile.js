9e207dfe16433db27d04b0fe939b5b1fc9d60120
Sandy <sandy@blue-geek.net>
2015-09-29 13:28:18 -0400

    updated GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,8 @@ module.exports = function (grunt) {
           }
         },
         files: {
-          'public/js/console-game.js': 'source/app.js'
+          'public/js/local-game.js'  : 'source/app.local.js'  ,
+          'public/js/remote-game.js' : 'source/app.remote.js'
         }
       }
     },
