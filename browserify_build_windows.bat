e2b0b106a9ef046abf3db229d8522536a422914c
Sandy <sandy@blue-geek.net>
2015-09-30 12:29:27 -0400

    Removed redundant Browserify .bat and .sh files
    Grunt does this

--- browserify_build_windows.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-ECHO creating local-game.js
-CALL browserify source\app.local.js > public\js\local-game.js
-ECHO local-game.js created
-ECHO creating remote-game.js
-CALL browserify source\app.remote.js > public\js\remote-game.js
-ECHO remote-game.js created
-PAUSE
