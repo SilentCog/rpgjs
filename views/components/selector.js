34d578a756c8a63a0bea1a44eab1c357593628d6
Mimi Flynn <mimiflynn@gmail.com>
2015-11-03 10:02:11 -0500

    switcher styles

--- views/components/selector.js
+++ views/components/selector.js
@@ -5,15 +5,29 @@ module.exports = React.createClass({
     handleSelect: React.PropTypes.func,
     selectValues: React.PropTypes.object
   },
-  handleClick: function (item) {
+  getInitialState: function () {
+    return {
+      activeIndex: 0
+    };
+  },
+  handleClick: function (item, index) {
     this.props.handleSelect(item);
+    this.setState({
+      activeIndex: index
+    });
   },
   render: function () {
     var that = this;
     var items = Object.keys(this.props.selectValues);
     items = items.map(function (item, index) {
       return (
-        <input key={item} type="button" value={item} name={item} onClick={that.handleClick.bind(that, item)} />
+        <input
+          key={item}
+          className={ (that.state.activeIndex === index) ? 'active' : '' }
+          type="button"
+          value={item}
+          name={item}
+          onClick={that.handleClick.bind(that, item, index)} />
       );
     });
     return (
